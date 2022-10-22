01bbeda44c4f42f71d02a619ec264a00b5a63594
Lee Jun <leejun.nakao@gmail.com>
2022-10-22 21:09:32 +0000

    test: create unit test for data-encryptor service

---
+++ src/services/data-encyptor.spec.ts
@@ -0,0 +1,13 @@
+import * as Service from "./data-encryptor";
+
+describe("Data encryptor service", () => {
+    it("Should encrypt provided data", () => {
+        const data = { foods: ["apple", "hamburguer", "melon"], colors: ["green", "red", "blue", "purple"]};
+        
+        const encoded = Service.encryptData(data);
+
+        const decoded = Service.decodeData(encoded);
+
+        expect(decoded.data).toEqual(data);
+    });
+});
