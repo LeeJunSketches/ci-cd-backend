fc02ae051f013ddd331b7695bb6d5b3a29c4084d
Lee Jun <leejun.nakao@gmail.com>
2022-10-22 21:36:39 +0000

    test: rename test

--- src/services/data-encyptor.spec.ts
+++ src/services/data-encyptor.spec.ts
@@ -1,7 +1,7 @@
 import * as Service from "./data-encryptor";
 
 describe("Data encryptor service", () => {
-    it("Should encrypt provided data", () => {
+    it("Should encrypt provided data correctly", () => {
         const data = { foods: ["apple", "hamburguer", "melon"], colors: ["green", "red", "blue", "purple"]};
         
         const encoded = Service.encryptData(data);
