c4d83dfa170f82f6afaa7ffffe46f73c995aebb6
Lee Jun <leejun.nakao@gmail.com>
2022-10-22 16:55:48 +0000

    feat: create server

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 import dotenv from "dotenv";
+import app from "src/server";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Running on port: ${PORT}`)
+})
+
 
 dotenv.config();
